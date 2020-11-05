0327ecffddaf32f9f3051137e006d51b3b368346
Satveer <satbar98@gmail.com>
2020-11-05 01:07:40 +0100

    Mein erster Commit

---
+++ TestGit1/TestGit1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestGit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("BYE");
+        }
+    }
+}
