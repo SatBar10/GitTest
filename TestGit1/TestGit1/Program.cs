c8046fca57870d5a73d1327b1601eef3780ceaa4
SatBar10 <72610546+SatBar10@users.noreply.github.com>
2020-11-06 14:20:02 +0100

    Update Program.cs

--- TestGit1/TestGit1/Program.cs
+++ TestGit1/TestGit1/Program.cs
@@ -6,7 +6,7 @@ namespace TestGit1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wor");
             Console.WriteLine("BYE");
         }
     }
