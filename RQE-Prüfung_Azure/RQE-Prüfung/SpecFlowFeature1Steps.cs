b972627b58065e6b61424eb73984977aa821d284
Satveer <satbar98@gmail.com>
2020-11-06 16:23:43 +0100

    new

---
+++ RQE-Prüfung_Azure/RQE-Prüfung/SpecFlowFeature1Steps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_Prüfung
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        int first, second, result;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            first = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            second = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = first + second;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
